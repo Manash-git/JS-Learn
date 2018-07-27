711b4fbc817ad774d346bceced7e0833dfaa5766
Manash-git <manashmondalcse@gmail.com>
2018-07-27 15:43:11 +0600

    call(), apply(), with/without parameter

--- OOP js/Bind-Call_Apply.js
+++ OOP js/Bind-Call_Apply.js
@@ -10,19 +10,50 @@ function printMe() {
 var obj1={
     name: "Manash Mondal",
     age: 25
-}
+};
 var obj2={
     name: "Casey Neistat",
     age: 40
+};
+
+//console.log(printMe());
+
+//var out1= printMe.bind(obj1);
+//console.log(out1());
+
+/**
+ * Call function done the bind work plus call the function
+ * 
+ */
+
+printMe.call(obj2);
+
+
+/***
+ * New Example for Apply()
+ * it is used for function binding with parameter
+ */
+function add(a,b) {
+    return (a+b)*this.c;
 }
 
-console.log(printMe());
+var multiplier1={
+    c:4
+};
+var multiplier2={
+    c:5
+};
 
-var out1= printMe.bind(obj1);
-console.log(out1());
+var result= add.call(multiplier1,10,2);     // call() with parameter 
+console.log(result);
 
+result= add.apply(multiplier2,[5,10]);      // Apply() parameter should be within an array
+console.log(result);
 
+// We can also use bind()
 
+var binded=add.bind(multiplier2);
+console.log(binded(2,3));
 
 
 
