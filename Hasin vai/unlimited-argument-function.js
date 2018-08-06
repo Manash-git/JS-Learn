ceac6113da8ea3afd5611c81e1ec950314f9250f
Manash-git <manashmondalcse@gmail.com>
2018-08-06 12:27:26 +0600

    spread push back

--- Hasin vai/unlimited-argument-function.js
+++ Hasin vai/unlimited-argument-function.js
@@ -16,7 +16,18 @@ function test(n,...params) {   // first one taking as our own way
     console.log(params.length);
     
 }
-test(1,3,5,7);
-test(1,0,5,7,4,2,8);
-test(1,1212,5,17,34,4442,48,4,2,4,6,67,8,787,99,09);
+// test(1,3,5,7);
+// test(1,0,5,7,4,2,8);
+// test(1,1212,5,17,34,4442,48,4,2,4,6,67,8,787,99,09);
 
+
+var students=[];
+
+addStudents(students,"Manash","Casey","McKinnon","MKBHD","Naveen");
+console.log(students.length);   // 5
+
+addStudents(students,"Manash","MKBHD","Naveen");
+console.log(students.length);   // 5+3 = 8
+
+addStudents(students,"Manash","MKBHD","Naveen","Shifat Ullah");
+console.log(students.length);   // 5+3+4 = 12
