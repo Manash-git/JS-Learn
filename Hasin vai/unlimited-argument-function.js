8b3ad6a63105e0f002d0179fdc0404be1d3e34a9
Manash-git <manashmondalcse@gmail.com>
2018-08-06 12:24:58 +0600

    one own parameter + spread

--- Hasin vai/unlimited-argument-function.js
+++ Hasin vai/unlimited-argument-function.js
@@ -5,13 +5,17 @@
  * 
  */
 
-
+/*
 function test(...params) {
     console.log(params.length);
     
 }
+*/
 
-
+function test(n,...params) {   // first one taking as our own way
+    console.log(params.length);
+    
+}
 test(1,3,5,7);
 test(1,0,5,7,4,2,8);
 test(1,1212,5,17,34,4442,48,4,2,4,6,67,8,787,99,09);
