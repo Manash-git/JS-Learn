3ccf3db3f3cf1e4427ceacdc1e1e344ee03c3e4f
Manash-git <manashmondalcse@gmail.com>
2018-08-06 12:21:26 +0600

    New || spread operator

---
+++ Hasin vai/unlimited-argument-function.js
@@ -0,0 +1,18 @@
+/**
+ * ES6 spread operator
+ * for taking unlimited argument in a function 
+ * ...{3 dots are used to defined this functionality}
+ * 
+ */
+
+
+function test(...params) {
+    console.log(params.length);
+    
+}
+
+
+test(1,3,5,7);
+test(1,0,5,7,4,2,8);
+test(1,1212,5,17,34,4442,48,4,2,4,6,67,8,787,99,09);
+
